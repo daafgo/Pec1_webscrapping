0e5741fd5982bfb35fc266f3089e211652a29f9d
Daniel Afonso González <daafgo@gmail.com>
2018-11-12 19:16:37 +0000

    output.csv contiene el valor de todos los juegos del catalogo de steam

--- webscrapper.py
+++ webscrapper.py
@@ -5,8 +5,7 @@ import pandas as pd
 i=1
 # df=pd.DataFrame( columns=['name', 'normalPrice', 'NewPrice'])
 titleCol=[]
-priceCol=[]
-priceDealCol=[]
+
 while True:
     url = 'http://store.steampowered.com/search/?sort_by=Released_DESC&page='+str(i)
     page = requests.get(url)
@@ -23,13 +22,17 @@ while True:
         info = {
                     "title": row.find(class_="title").text,
                     "price": price,
-                    "priceNew":priceNew
+                    "priceNew":priceNew,
+                    "releaseDate":row.find(class_="search_released").text,
+                    "profileLink":row['href']
+
                     }
                     
         titleCol.append(info)
     i = i+1
-    if (i>5):
+    print ("analizando pagina: ",i)
+    if ((not row) or i>1950):
         break
 df=pd.DataFrame(titleCol)
 print df
-df.to_csv("out.csv", encoding='utf-8')
+df.to_csv("out.csv",sep=';',  encoding='utf-8')
